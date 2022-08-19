6c4f3e325101a0d3b0b30a07506c4ce0fb5a2042
Noela-Murugi <murugin254@gmail.com>
2022-08-19 21:01:42 +0300

    DOM query selector practice

---
+++ index.js
@@ -0,0 +1,6 @@
+const li2 = document.querySelector("ul.ranked-list li ul li");
+li2;
+
+const div4 = document.querySelector("ul.unranked-list li div");
+div4;
+document.getElementById("app").querySelectorAll("ul.ranked-list li");
